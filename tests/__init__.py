ce66bb027866d4921407bf57e4fb4189fb222d76
Barry Xu 许庭新 <barry.xu@nio.com>
2019-07-12 09:48:21 +0800

    improve import

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
-import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import sys
 
-import pysqler
+file_path = os.path.join(os.path.dirname(__file__), '..')
+abs_path = os.path.abspath(file_path)
+sys.path.insert(0, abs_path)
